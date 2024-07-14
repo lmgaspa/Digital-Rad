220b0c69ec70ae57f28ce8904404852bd758d17d
Luiz <seu-email@example.com>
2024-07-14 10:47:43 -0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Digital-Rad',
+  base: '/Digital-Rad/',
   plugins: [react()],
   server: {
     open: true,
