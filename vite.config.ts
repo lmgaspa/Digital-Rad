d4bc1793661e0cf75ffd505be3c2527768c543bb
Luiz <seu-email@example.com>
2024-07-14 12:06:58 -0300

    try

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Digital-Rad/',
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+  base: '/',
+})
