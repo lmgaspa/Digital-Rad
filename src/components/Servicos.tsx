0bf141bd64445b15f779c8ae6cb4cc53c1d3ad82
lmgaspa <luhmgasparetto@gmail.com>
2025-03-24 11:52:21 -0300

    fix'

--- src/components/Servicos.tsx
+++ src/components/Servicos.tsx
@@ -30,8 +30,7 @@ export default function Servicos() {
             serviço.</h3>
           <h3>
             Dentre nossos parceiros estão a Irmandade da Santa Casa de Misericórdia Nossa Senhora Das
-            Graças - Piên, Clínica de Diagnóstico Capão Raso, Clínica Dr. Mantovani, entre outros no interior do Estado
-            Do Paraná.</h3>
+            Graças - Piên, Clínica de Diagnóstico Capão Raso, Clínica Dr. Mantovani e da Clínica Cajuru.</h3>
           <h3>
             Além de mão de obra especializada, contamos com o serviço de telerradiologia que permite o
             diagnóstico à distância, emitindo, portanto, uma segunda opinião técnica através do envio digital das
